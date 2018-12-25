b2783a93d3ed7939900dbb666c093d507823b389
Raghavendra <rag1261991@gmail.com>
2018-12-25 21:47:17 +0530

    Adding Assert statements to test steps

--- src/main/java/com/cleartrip/tests/HotelBookingTest.java
+++ src/main/java/com/cleartrip/tests/HotelBookingTest.java
@@ -27,12 +27,15 @@ public class HotelBookingTest extends BaseClass {
     @Test
     public void shouldBeAbleToSearchForHotels() throws IOException {
     	logger = extent.createTest("shouldBeAbleToSearchForHotels");
-        cm.clickOn(hp.hotelLink, logger);
-        cm.enterTextInTextbox(hp.localityTextBox, logger, "Indiranagar, Bangalore");
-        cm.clickOn(hp.checkInDate, logger);
-        cm.clickOn(hp.checkOutDate, logger);
-        cm.selectFromVisibleText(hp.travellerSelection, logger, "1 room, 2 adults");
-        cm.clickOn(hp.searchButton, logger);
+        Assert.assertTrue(cm.clickOn(hp.hotelLink, logger), "Not able to click element");
+        Assert.assertTrue(cm.enterTextInTextbox(hp.localityTextBox, logger, "Indiranagar, Bangalore"), "Not able to enter text");
+        Assert.assertTrue(cm.waitFor(2000, logger), "Not able to wait for element");
+        Assert.assertTrue(cm.pressEnter(logger), "Not able to press escape");
+        Assert.assertTrue(cm.clickOn(hp.datepicker, logger), "Not able to click element");
+        Assert.assertTrue(cm.clickOn(hp.checkInDate, logger), "Not able to click element");
+        Assert.assertTrue(cm.clickOn(hp.checkOutDate, logger), "Not able to click element");
+        Assert.assertTrue(cm.selectFromVisibleText(hp.travellerSelection, logger, "1 room, 2 adults"), "Not able to select element");
+        Assert.assertTrue(cm.clickOn(hp.searchButton, logger), "Not able to click element");
     }
     
     @AfterTest

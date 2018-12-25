5b07fcf213912fc5e574e891c743897ca6d0963b
Raghavendra <rag1261991@gmail.com>
2018-12-25 21:46:05 +0530

    Changed methods return type to boolean.

--- src/main/java/com/cleartrip/util/CommonMethods.java
+++ src/main/java/com/cleartrip/util/CommonMethods.java
@@ -1,13 +1,18 @@
 package com.cleartrip.util;
 
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
 import java.io.File;
 import java.io.IOException;
+
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.io.FileHandler;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.MediaEntityBuilder;
@@ -22,16 +27,18 @@ public class CommonMethods {
 		this.driver=driver;
 	}
 
-	public void waitFor(int durationInMilliSeconds, ExtentTest logger) throws IOException {
+	public boolean waitFor(int durationInMilliSeconds, ExtentTest logger) throws IOException {
         try {
             Thread.sleep(durationInMilliSeconds);
             logger.log(Status.PASS,"wait for "+durationInMilliSeconds);
         } catch (InterruptedException e) {
         	logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+        	return false;
         }
+        return true;
     }
 	
-	public void clickOn(WebElement we, ExtentTest logger) throws IOException{
+	public boolean clickOn(WebElement we, ExtentTest logger) throws IOException{
         try {
         	WebDriverWait wait=new WebDriverWait(driver,20);
         	wait.until(ExpectedConditions.elementToBeClickable(we));
@@ -39,27 +46,71 @@ public class CommonMethods {
         	we.click();
       } catch (Exception e) {
     	    logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
-      }		
+    	    return false;
+      }	
+        return true;
 	}
 
-	public void waitForIFrameAdSwitch(WebElement we, ExtentTest logger) throws IOException{			
+	public boolean enterTextInTextbox(WebElement we, ExtentTest logger,String text) throws IOException{
+        try {
+        	WebDriverWait wait=new WebDriverWait(driver,20);
+        	wait.until(ExpectedConditions.visibilityOf(we)).sendKeys(text);
+        	logger.log(Status.PASS,text+" :text is entered");
+      } catch (Exception e) {
+    	    logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+    	    return false;
+      }		
+        return true;
+	}
+	
+	public boolean selectFromVisibleText(WebElement we, ExtentTest logger,String text) throws IOException{
+        try {
+        	WebDriverWait wait=new WebDriverWait(driver,20);
+        	wait.until(ExpectedConditions.visibilityOf(we));
+        	Select sel = new Select(we);
+        	sel.selectByVisibleText(text);
+        	logger.log(Status.PASS,text+" :text is Selected");
+      } catch (Exception e) {
+    	    logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+    	    return false;
+      }		
+        return true;
+	}
+	
+	public boolean pressEnter(ExtentTest logger) throws IOException{
+		try{
+			Robot r = new Robot();
+			r.keyPress(KeyEvent.VK_ENTER);
+			r.keyRelease(KeyEvent.VK_ENTER);
+			logger.log(Status.PASS," Enter is Pressed");
+		}catch(Exception e){
+			logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+    	    return false;
+		}
+		return true;
+	}
+	public boolean waitForIFrameAdSwitch(WebElement we, ExtentTest logger) throws IOException{			
         try {
         	WebDriverWait wait=new WebDriverWait(driver,20);
     		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(we));
     		logger.log(Status.PASS,"Switched to frame:");
       } catch (Exception e) {
     	  logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+    	  return false;
       }	
+        return true;
 	}
 
-	public void waitTillElementIsVisible(WebElement we, ExtentTest logger) throws IOException{			
+	public boolean waitTillElementIsVisible(WebElement we, ExtentTest logger) throws IOException{			
         try {
         	WebDriverWait wait=new WebDriverWait(driver,20);
     		wait.until(ExpectedConditions.visibilityOf(we));
     		logger.log(Status.PASS,"Element:"+we.getText()+" is visible");
       } catch (Exception e) {
     	  logger.fail(e.toString(), MediaEntityBuilder.createScreenCaptureFromPath(getScreenshot()).build());
+    	  return false;
       }	
+        return true;
 	}
 	
 	public String getScreenshot(){

8ad2f741a2a1cd2ee79423c6672097bccbb42926
Raghavendra <rag1261991@gmail.com>
2018-12-25 18:44:47 +0530

    Used recently created POM elements for homepage in test case and added latest chromedriver since signon frame was not detecting

--- src/main/java/com/cleartrip/tests/SignInTest.java
+++ src/main/java/com/cleartrip/tests/SignInTest.java
@@ -1,32 +1,40 @@
 package com.cleartrip.tests;
+import com.cleartrip.pages.HomePage;
 import com.sun.javafx.PlatformUtil;
 import com.util.BaseClass;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
 public class SignInTest extends BaseClass{
 
+	HomePage hp;
+	
 	@BeforeTest
 	public void setup(){
 		setDriverPath();
 		startBrowser();
+		hp = new HomePage(driver);
 	}
 	
 	@Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
 
         waitFor(2000);
-        driver.findElement(By.linkText("Your trips")).click();
-        driver.findElement(By.id("SignIn")).click();
-
-        driver.findElement(By.id("signInButton")).click();
-
-        String errors1 = driver.findElement(By.id("errors1")).getText();
+        hp.yourTrips.click();
+        hp.SignIn.click();
+        waitFor(2000);
+        WebDriverWait wait=new WebDriverWait(driver,20);
+   		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(hp.iFrame));
+        hp.signInButton.click();
+        waitFor(500);
+        String errors1 = hp.signinErrorMessage.getText();
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }

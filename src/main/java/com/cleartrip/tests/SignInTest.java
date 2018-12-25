94d1f9d6fc0ff8953119c75b26b958c131db5091
Raghavendra <rag1261991@gmail.com>
2018-12-25 19:12:07 +0530

    Configuring and using common methods in test page, removed waitfor method and added in common methods file.

--- src/main/java/com/cleartrip/tests/SignInTest.java
+++ src/main/java/com/cleartrip/tests/SignInTest.java
@@ -2,6 +2,7 @@ package com.cleartrip.tests;
 import com.cleartrip.pages.HomePage;
 import com.sun.javafx.PlatformUtil;
 import com.util.BaseClass;
+import com.util.CommonMethods;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,35 +16,27 @@ import org.testng.annotations.Test;
 public class SignInTest extends BaseClass{
 
 	HomePage hp;
-	
+	CommonMethods cm;
 	@BeforeTest
 	public void setup(){
 		setDriverPath();
 		startBrowser();
 		hp = new HomePage(driver);
+		cm = new CommonMethods(driver);
 	}
 	
 	@Test
     public void shouldThrowAnErrorIfSignInDetailsAreMissing() {
-
-        waitFor(2000);
-        hp.yourTrips.click();
-        hp.SignIn.click();
-        waitFor(2000);
-        WebDriverWait wait=new WebDriverWait(driver,20);
-   		wait.until(ExpectedConditions.frameToBeAvailableAndSwitchToIt(hp.iFrame));
-        hp.signInButton.click();
-        waitFor(500);
+        cm.clickOn(hp.yourTrips);
+        cm.clickOn(hp.SignIn);
+   		cm.waitForIFrameAdSwitch(hp.iFrame);
+        cm.clickOn(hp.signInButton);
+        cm.waitTillElementIsVisible(hp.signinErrorMessage);
         String errors1 = hp.signinErrorMessage.getText();
+        System.out.println(errors1);
         Assert.assertTrue(errors1.contains("There were errors in your submission"));
         driver.quit();
     }
 
-    private void waitFor(int durationInMilliSeconds) {
-        try {
-            Thread.sleep(durationInMilliSeconds);
-        } catch (InterruptedException e) {
-            e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
-        }
-    }
+    
 }
